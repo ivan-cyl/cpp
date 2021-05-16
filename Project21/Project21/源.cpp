9f468c3f34eec240addd03c23caded4c421dc1ff
ivan-cyl <78088602+ivan-cyl@users.noreply.github.com>
2021-05-16 12:50:57 +0800

    Add files via upload

---
+++ Project21/Project21/源.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	//int a, b, c;
+	int x = 3, y = -4, z = 5;
+	printf((x&&y) == (x || z));
+	!(x > y) + (y != z) || (x = y) && (y - z);
+	x++ - y + (++z);
+
+	system("pause");
+	return 0;
+}
